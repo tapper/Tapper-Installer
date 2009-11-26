be4d28a2d122a14d850d3adb566b3c7b7d1f1587
Maik Hentsche <maik.hentsche@amd.com>
2009-11-26 13:57:20 +0100

    Installer: added scenario id to generated PRC config

--- lib/Artemis/Installer/Precondition/PRC.pm
+++ lib/Artemis/Installer/Precondition/PRC.pm
@@ -62,6 +62,7 @@ method create_config($prc)
         $config->{test_run}        = $self->{cfg}->{test_run};
         $config->{port}            = $self->{cfg}->{mcp_port} if $self->{cfg}->{mcp_port};
         $config->{prc_nfs_server}  = $self->{cfg}->{prc_nfs_server} if $self->{cfg}->{prc_nfs_server}; # prc_nfs_path is set by merging paths above
+        $config->{scenario_id}     = $self->{cfg}->{scenario_id} if $self->{cfg}->{scenario_id};
 
         return (0, $config);
 };
