ce77a190cba96652cf9edff493d7ca0ae7c862f8
Maik Hentsche <maik.hentsche@amd.com>
2010-04-01 15:57:27 +0200

    Installer: added script to start Installer in simnow case

---
+++ bin/artemis-installer-simnow.pl
@@ -0,0 +1,32 @@
+#!/opt/artemis/bin/perl
+
+use warnings;
+use strict;
+use Log::Log4perl;
+use Artemis::Installer::Base;
+
+BEGIN {
+	Log::Log4perl::init('/etc/log4perl.cfg');
+}
+
+
+my $client = new Artemis::Installer::Base;
+$client->system_install("simnow");
+
+
+=pod
+
+=head1 NAME
+
+artemis-installer-client.pl - control the installation and setup of an automatic test system
+
+=head1 SYNOPSIS
+
+artemis-installer-client.pl 
+
+=head1 DESCRIPTION
+
+This program is the start script of the Artemis::Installer project. It calls
+Artemis::Installer::Base which cares for the rest.
+
+=cut
